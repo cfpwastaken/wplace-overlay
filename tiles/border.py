1af83f6df13a4c9372cba08e6a9adf532a5344a4
Cfp <chaosflo44plus@gmail.com>
2025-07-31 12:30:43 +0200

    feat: center the pixels

--- tiles/border.py
+++ tiles/border.py
@@ -5,17 +5,17 @@ import sys
 img = Image.open(sys.argv[1] + "_orig.png").convert("RGBA")
 w, h = img.size
 
-# Create new image with spacing
-new_w = w * 2
-new_h = h * 2
+# Create new 3x image with 1px border around each pixel
+new_w = w * 3
+new_h = h * 3
 new_img = Image.new("RGBA", (new_w, new_h), (0, 0, 0, 0))  # transparent background
 
-# Copy each pixel to its new position
+# Place each pixel with a 1px border on all sides
 for y in range(h):
-    for x in range(w):
-        pixel = img.getpixel((x, y))
-        new_img.putpixel((x * 2, y * 2), pixel)
+	for x in range(w):
+		pixel = img.getpixel((x, y))
+		new_img.putpixel((x * 3 + 1, y * 3 + 1), pixel)
 
-# Save it
-new_img = new_img.resize((2000, 2000), resample=Image.NEAREST)
+# Resize to 3000x3000
+new_img = new_img.resize((3000, 3000), resample=Image.NEAREST)
 new_img.save(sys.argv[1] + ".png")
