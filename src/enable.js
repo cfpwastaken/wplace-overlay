8d7972aecf8505be4768c8b86332feeb0a80face
Jannik <jannik@picoscratch.de>
2025-10-07 17:04:31 +0200

    feat: animate overlay menu

--- src/enable.js
+++ src/enable.js
@@ -324,7 +324,16 @@ function patchUI() {
 		closeButton.textContent = "X";
 		closeButton.classList = "btn btn-square shadow-md";
 		closeButton.style.marginLeft = "auto";
-		closeButton.addEventListener("click", () => {
+		closeButton.addEventListener("click", async () => {
+			overlayOptions.animate([
+				{ opacity: 1, transform: "translate(50%, -50%)" },
+				{ opacity: 0, transform: "translate(50%, -40%)" }
+			], {
+				duration: 200,
+				easing: "ease-out",
+				fill: "forwards"
+			});
+			await new Promise(resolve => setTimeout(resolve, 200));
 			overlayOptions.style.display = "none";
 		});
 		header.appendChild(closeButton);
@@ -444,11 +453,28 @@ function patchUI() {
 	overlayButton.title = "Overlay Options";
 	overlayButton.id = "overlay-button";
 	overlayButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path d="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>';
-	overlayButton.addEventListener("click", () => {
+	overlayButton.addEventListener("click", async () => {
 		const overlayOptions = document.getElementById("overlay-options");
 		if (overlayOptions.style.display === "none") {
 			overlayOptions.style.display = "flex";
+			overlayOptions.animate([
+				{ opacity: 0, transform: "translate(50%, -40%)" },
+				{ opacity: 1, transform: "translate(50%, -50%)" }
+			], {
+				duration: 200,
+				easing: "ease-out",
+				fill: "forwards"
+			});
 		} else {
+			overlayOptions.animate([
+				{ opacity: 1, transform: "translate(50%, -50%)" },
+				{ opacity: 0, transform: "translate(50%, -40%)" }
+			], {
+				duration: 200,
+				easing: "ease-out",
+				fill: "forwards"
+			});
+			await new Promise(resolve => setTimeout(resolve, 200));
 			overlayOptions.style.display = "none";
 		}
 	});
