6028131f3c207e5947749274c9ad3d0621f9726d
Jannik <jannik@picoscratch.de>
2025-10-07 15:30:03 +0200

    feat: add overlay menu

--- src/enable.js
+++ src/enable.js
@@ -213,11 +213,11 @@ function updateOverlayMode() {
 	if (map.getSource("overlay")) {
 		map.removeSource("overlay");
 	}
-	if (map.getSource("darken")) {
-		map.removeSource("darken");
-	}
 	if (map.getLayer("darken")) {
 		map.removeLayer("darken");
+	}
+	if (map.getSource("darken")) {
+		map.removeSource("darken");
 	}
 
 	if(overlayMode === "off" || overlayMode === "difference" || overlayMode === "out") {
@@ -271,99 +271,152 @@ function updateOverlayMode() {
 const symbolList = ["Black", "Dark Gray", "Gray", "Medium Gray", "Light Gray", "White", "Deep Red", "Dark Red", "Red", "Light Red", "Dark Orange", "Orange", "Gold", "Yellow", "Light Yellow", "Dark Goldenrod", "Goldenrod", "Light Goldenrod", "Dark Olive", "Olive", "Light Olive", "Dark Green", "Green", "Light Green", "Dark Teal", "Teal", "Light Teal", "Dark Cyan", "Cyan", "Light Cyan", "Dark Blue", "Blue", "Light Blue", "Dark Indigo", "Indigo", "Light Indigo", "Dark Slate Blue", "Slate Blue", "Light Slate Blue", "Dark Purple", "Purple", "Light Purple", "Dark Pink", "Pink", "Light Pink", "Dark Peach", "Peach", "Light Peach", "Dark Brown", "Brown", "Light Brown", "Dark Tan", "Tan", "Light Tan", "Dark Beige", "Beige", "Light Beige", "Dark Stone", "Stone", "Light Stone", "Dark Slate", "Slate", "Light Slate"];
 
 function patchUI() {
-	if (document.getElementById("overlay-blend-button")) {
+	if (document.getElementById("overlay-button")) {
 		return; // Button already exists, no need to patch again
 	}
-	let blendButton = document.createElement("button");
-	blendButton.id = "overlay-blend-button";
-	blendButton.textContent = "Overlay: " + overlayMode.charAt(0).toUpperCase() + overlayMode.slice(1);
-	blendButton.style.backgroundColor = "#0e0e0e7f";
-	blendButton.style.color = "white";
-	blendButton.style.border = "solid";
-	blendButton.style.borderColor = "#1d1d1d7f";
-	blendButton.style.borderRadius = "4px";
-	blendButton.style.padding = "5px 10px";
-	blendButton.style.cursor = "pointer";
-	blendButton.style.backdropFilter = "blur(2px)";
+	if (!document.querySelector("#overlay-options")) {
+		let overlayOptions = document.createElement("div");
+		overlayOptions.id = "overlay-options";
+		overlayOptions.style.position = "fixed";
+		overlayOptions.style.top = "50%";
+		overlayOptions.style.right = "50%";
+		overlayOptions.style.transform = "translate(50%, -50%)";
+		overlayOptions.style.width = "50%";
+		overlayOptions.style.zIndex = "1000";
+		overlayOptions.style.display = "none";
+		overlayOptions.style.flexDirection = "column";
+		overlayOptions.style.gap = "10px";
+		overlayOptions.style.backgroundColor = "#0e0e0e7f";
+		overlayOptions.style.padding = "10px";
+		overlayOptions.style.borderRadius = "8px";
+		overlayOptions.style.backdropFilter = "blur(15px)";
+		document.body.appendChild(overlayOptions);
 
-	blendButton.addEventListener("click", () => {
-		overlayMode = OVERLAY_MODES[(OVERLAY_MODES.indexOf(overlayMode) + 1) % OVERLAY_MODES.length];
-		if (IS_TAMPERMONKEY) GM_setValue("OVERLAY_MODE", overlayMode);
-		blendButton.textContent = `Overlay: ${overlayMode.charAt(0).toUpperCase() + overlayMode.slice(1)}`;
-		console.log("Overlay mode set to:", overlayMode);
-		updateOverlayMode();
-	});
+		let header = document.createElement("h2");
+		header.textContent = "Wplace Overlay";
+		header.style.color = "white";
+		header.style.fontSize = "1.5em";
+		header.style.fontWeight = "bold";
+		header.style.display = "flex";
+		header.style.alignItems = "center";
+		overlayOptions.appendChild(header);
 
-	let darkenMode = document.createElement("button");
-	darkenMode.textContent = `Darken: ${darken ? "On" : "Off"}`;
-	darkenMode.style.backgroundColor = "#0e0e0e7f";
-	darkenMode.style.color = "white";
-	darkenMode.style.border = "solid";
-	darkenMode.style.borderColor = "#1d1d1d7f";
-	darkenMode.style.borderRadius = "4px";
-	darkenMode.style.padding = "5px 10px";
-	darkenMode.style.cursor = "pointer";
-	darkenMode.style.backdropFilter = "blur(2px)";
+		let closeButton = document.createElement("button");
+		closeButton.textContent = "X";
+		closeButton.classList = "btn btn-square shadow-md";
+		closeButton.style.marginLeft = "auto";
+		closeButton.addEventListener("click", () => {
+			overlayOptions.style.display = "none";
+		});
+		header.appendChild(closeButton);
 
-	darkenMode.addEventListener("click", () => {
-		darken = !darken;
-		if(IS_TAMPERMONKEY) GM_setValue("DARKEN", darken);
-		darkenMode.textContent = `Darken: ${darken ? "On" : "Off"}`;
-		console.log("Darken mode set to:", darken);
-		updateOverlayMode();
-	});
+		let blendButton = document.createElement("button");
+		blendButton.id = "overlay-blend-button";
+		blendButton.textContent = "Overlay: " + overlayMode.charAt(0).toUpperCase() + overlayMode.slice(1);
+		blendButton.style.backgroundColor = "#0e0e0e7f";
+		blendButton.style.color = "white";
+		blendButton.style.border = "solid";
+		blendButton.style.borderColor = "#1d1d1d7f";
+		blendButton.style.borderRadius = "4px";
+		blendButton.style.padding = "5px 10px";
+		blendButton.style.cursor = "pointer";
+		blendButton.style.backdropFilter = "blur(2px)";
 
-	let styleButton = document.createElement("button");
-	styleButton.textContent = `Style: ${currentStyle.charAt(0).toUpperCase() + currentStyle.slice(1)}`;
-	styleButton.style.backgroundColor = "#0e0e0e7f";
-	styleButton.style.color = "white";
-	styleButton.style.border = "solid";
-	styleButton.style.borderColor = "#1d1d1d7f";
-	styleButton.style.borderRadius = "4px";
-	styleButton.style.padding = "5px 10px";
-	styleButton.style.cursor = "pointer";
-	styleButton.style.backdropFilter = "blur(2px)";
-
-	styleButton.addEventListener("click", async () => {
-		const map = getMap();
-		currentStyle = STYLES[(STYLES.indexOf(currentStyle) + 1) % STYLES.length];
-		styleButton.textContent = `Style: ${currentStyle.charAt(0).toUpperCase() + currentStyle.slice(1)}`;
-		const style = map.getStyle();
-		const pixelSource = style.sources["pixel-art-layer"];
-		const hoverSource = map.getSource("pixel-hover");
-		const pixelLayer = style.layers.find(l => l.id === "pixel-art-layer");
-		const hoverLayer = style.layers.find(l => l.id === "pixel-hover");
-		map.setStyle(await fetch("https://maps.wplace.live/styles/" + currentStyle).then(res => res.json()));
-		console.log(hoverSource, hoverLayer);
-		map.once("styledata", () => {
-			if (pixelSource && !map.getSource("pixel-art-layer")) {
-				map.addSource("pixel-art-layer", pixelSource);
-			}
-			if (hoverSource && !map.getSource("pixel-hover")) {
-				map.addSource("pixel-hover", {
-					type: "canvas",
-					canvas: hoverSource.canvas,
-					coordinates: hoverSource.coordinates,
-				});
-			}
-			if (pixelLayer && !map.getLayer("pixel-art-layer")) {
-				map.addLayer(pixelLayer);
-			}
-			if (hoverLayer && !map.getLayer("pixel-hover")) {
-				map.addLayer(hoverLayer);
-			}
-
+		blendButton.addEventListener("click", () => {
+			overlayMode = OVERLAY_MODES[(OVERLAY_MODES.indexOf(overlayMode) + 1) % OVERLAY_MODES.length];
+			if (IS_TAMPERMONKEY) GM_setValue("OVERLAY_MODE", overlayMode);
+			blendButton.textContent = `Overlay: ${overlayMode.charAt(0).toUpperCase() + overlayMode.slice(1)}`;
+			console.log("Overlay mode set to:", overlayMode);
 			updateOverlayMode();
 		});
+
+		let darkenMode = document.createElement("button");
+		darkenMode.textContent = `Darken: ${darken ? "On" : "Off"}`;
+		darkenMode.style.backgroundColor = "#0e0e0e7f";
+		darkenMode.style.color = "white";
+		darkenMode.style.border = "solid";
+		darkenMode.style.borderColor = "#1d1d1d7f";
+		darkenMode.style.borderRadius = "4px";
+		darkenMode.style.padding = "5px 10px";
+		darkenMode.style.cursor = "pointer";
+		darkenMode.style.backdropFilter = "blur(2px)";
+
+		darkenMode.addEventListener("click", () => {
+			darken = !darken;
+			if(IS_TAMPERMONKEY) GM_setValue("DARKEN", darken);
+			darkenMode.textContent = `Darken: ${darken ? "On" : "Off"}`;
+			console.log("Darken mode set to:", darken);
+			updateOverlayMode();
+		});
+
+		let styleButton = document.createElement("button");
+		styleButton.textContent = `Style: ${currentStyle.charAt(0).toUpperCase() + currentStyle.slice(1)}`;
+		styleButton.style.backgroundColor = "#0e0e0e7f";
+		styleButton.style.color = "white";
+		styleButton.style.border = "solid";
+		styleButton.style.borderColor = "#1d1d1d7f";
+		styleButton.style.borderRadius = "4px";
+		styleButton.style.padding = "5px 10px";
+		styleButton.style.cursor = "pointer";
+		styleButton.style.backdropFilter = "blur(2px)";
+
+		styleButton.addEventListener("click", async () => {
+			const map = getMap();
+			currentStyle = STYLES[(STYLES.indexOf(currentStyle) + 1) % STYLES.length];
+			styleButton.textContent = `Style: ${currentStyle.charAt(0).toUpperCase() + currentStyle.slice(1)}`;
+			const style = map.getStyle();
+			const pixelSource = style.sources["pixel-art-layer"];
+			const hoverSource = map.getSource("pixel-hover");
+			const pixelLayer = style.layers.find(l => l.id === "pixel-art-layer");
+			const hoverLayer = style.layers.find(l => l.id === "pixel-hover");
+			map.setStyle(await fetch("https://maps.wplace.live/styles/" + currentStyle).then(res => res.json()));
+			console.log(hoverSource, hoverLayer);
+			map.once("styledata", () => {
+				if (pixelSource && !map.getSource("pixel-art-layer")) {
+					map.addSource("pixel-art-layer", pixelSource);
+				}
+				if (hoverSource && !map.getSource("pixel-hover")) {
+					map.addSource("pixel-hover", {
+						type: "canvas",
+						canvas: hoverSource.canvas,
+						coordinates: hoverSource.coordinates,
+					});
+				}
+				if (pixelLayer && !map.getLayer("pixel-art-layer")) {
+					map.addLayer(pixelLayer);
+				}
+				if (hoverLayer && !map.getLayer("pixel-hover")) {
+					map.addLayer(hoverLayer);
+				}
+
+				updateOverlayMode();
+			});
+		});
+
+		overlayOptions.appendChild(blendButton);
+		overlayOptions.appendChild(darkenMode);
+		overlayOptions.appendChild(styleButton);
+	}
+
+	let overlayButton = document.createElement("button");
+	overlayButton.classList = "btn btn-square shadow-md";
+	overlayButton.title = "Overlay Options";
+	overlayButton.id = "overlay-button";
+	overlayButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path d="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>';
+	overlayButton.addEventListener("click", () => {
+		const overlayOptions = document.getElementById("overlay-options");
+		if (overlayOptions.style.display === "none") {
+			overlayOptions.style.display = "flex";
+		} else {
+			overlayOptions.style.display = "none";
+		}
 	});
 
 	const buttonContainer = document.querySelector("div.gap-4:nth-child(1) > div:nth-child(2)");
 	const leftSidebar = document.querySelector("html body div div.disable-pinch-zoom.relative.h-full.overflow-hidden.svelte-6wmtgk div.absolute.right-2.top-2.z-30 div.flex.flex-col.gap-4.items-center");
 	const paintMenu = document.querySelector("div.rounded-t-box > div.relative.px-3 > div.mb-4.mt-3 div")
 	if (buttonContainer) {
-		buttonContainer.appendChild(blendButton);
-		buttonContainer.appendChild(darkenMode);
-		buttonContainer.appendChild(styleButton);
+		buttonContainer.appendChild(overlayButton);
 		buttonContainer.classList.remove("items-center");
 		buttonContainer.classList.add("items-end");
 	}
